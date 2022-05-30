b114f29897430803ab40f785aba04b8e4553df94
Henri Lunnikivi <heggggxa@gmail.com>
2022-05-30 15:02:47 +0300

    Remove subsys hack

--- ballast-sysctrl-pac/update.sh
+++ ballast-sysctrl-pac/update.sh
@@ -22,10 +22,4 @@ cargo fmt
 grep -E 'feature = "rt"|extern crate riscv_rt' src/lib.rs | tee librs-patch
 grep -Ev 'feature = "rt"|extern crate riscv_rt' src/lib.rs > librs-temp && mv librs-temp src/lib.rs
 
-# Re-export the local processor subsystem with a special name (author: Henri Lunnikivi)
-echo "/// Re-export the local processor subsystem with a special name. This allows us to refer to" >> src/lib.rs
-echo "/// the current processor from all distinct cores." >> src/lib.rs
-echo "#[allow(unused_imports)]" >> src/lib.rs
-echo "pub use crate::{sysctrl as cpu_subsys, SYSCTRL as CPU_SUBSYS};" >> src/lib.rs
-
 cargo check
